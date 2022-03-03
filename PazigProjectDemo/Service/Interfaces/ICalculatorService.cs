df6c6934a10a27a4fbbff2cdc4e36d3681750ff7
Agnieszka Różańska <agniroz767@student.polsl.pl>
2022-03-03 15:49:10 +0100

    Create ICalculatorService

---
+++ PazigProjectDemo/Service/Interfaces/ICalculatorService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PazigProjectDemo.Service.Interfaces
+{
+    public interface ICalculatorService
+    {
+        double Add(double number1, double number2);
+
+        double Minus(double number1, double number2);
+
+        double Multiplication(double number1, double number2);
+    }
+}
