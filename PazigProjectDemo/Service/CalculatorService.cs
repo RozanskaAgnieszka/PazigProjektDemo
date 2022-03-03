31506e8af062c637c6f660a663536d5ce7fe0bbe
Agnieszka Różańska <agniroz767@student.polsl.pl>
2022-03-03 15:37:58 +0100

    Created empty class CalculatorService

---
+++ PazigProjectDemo/Service/CalculatorService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PazigProjectDemo.Service
+{
+    public sealed class CalculatorService
+    {
+    }
+}
