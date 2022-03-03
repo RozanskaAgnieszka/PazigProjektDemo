b980699e5cf4bb9a155c2e093d6e49da8ad63354
Agnieszka Różańska <agniroz767@student.polsl.pl>
2022-03-03 15:55:23 +0100

    Implementation functions Add, Minus and Multiplication

--- PazigProjectDemo/Service/CalculatorService.cs
+++ PazigProjectDemo/Service/CalculatorService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PazigProjectDemo.Service.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,21 @@ using System.Threading.Tasks;
 
 namespace PazigProjectDemo.Service
 {
-    public sealed class CalculatorService
+    public sealed class CalculatorService : ICalculatorService
     {
+        public double Add(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+
+        public double Minus(double number1, double number2)
+        {
+            return number1 - number2;
+        }
+
+        public double Multiplication(double number1, double number2)
+        {
+            return number1 * number2;
+        }
     }
 }
